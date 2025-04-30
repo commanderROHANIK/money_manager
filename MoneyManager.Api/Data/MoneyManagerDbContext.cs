8fd1d81eb1d6f191fb8ee07c73b01f13d95d36ff
Balázs Kerekes <kerekes.balazs.12@gmail.com>
2025-04-30 11:47:15 +0200

    create database

---
+++ MoneyManager.Api/Data/MoneyManagerDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using MoneyManager.Api.Models;
+
+namespace MoneyManager.Api.Data
+{
+    public class MoneyManagerDbContext : DbContext
+    {
+        public MoneyManagerDbContext(DbContextOptions<MoneyManagerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BankAccount> BankAccounts { get; set; }
+
+        public DbSet<Loan> Loans { get; set; }
+
+        public DbSet<RentalProperty> RentalProperties { get; set; }
+
+        public DbSet<Stock> Stocks { get; set; }
+
+        public DbSet<UpcomingEvent> UpcomingEvents { get; set; }
+
+
+        // We'll add DbSet<> properties later like:
+        // public DbSet<BankAccount> BankAccounts { get; set; }
+        // public DbSet<Loan> Loans { get; set; }
+    }
+}
